2751fd7efe19fe9dc1c55ff96d153691c582995e
pdrolromero <75053393+pdrolromero@users.noreply.github.com>
2021-09-01 21:44:24 -0400

    crear remolques para vehiculos ya existentes

--- fleet_additionals/__manifest__.py
+++ fleet_additionals/__manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Fleet',
-    'version': '1.0.20210901.9',
+    'version': '1.1.20210901.1',
     
     # any module necessary for this one to work correctly
     'depends': ['fleet', 'industry_fsm', 'website_dispatch_requests'],
@@ -21,6 +21,7 @@
     # always loaded
     'data': [
 
+        'data/action_create_remolque.xml',
         'security/ir.model.access.csv',
         'views/fleet_vehicle_views.xml',
         'views/remolque_dia_views.xml',
--- fleet_additionals/models/fleet_vehicle.py
+++ fleet_additionals/models/fleet_vehicle.py
@@ -25,20 +25,43 @@ class FleetVehicle(models.Model):
         day = 0
         while day <= cant_days:
             fecha = today + timedelta(days=day)
-
             remolque_dia.create({
                 'patente_remolque': vehicle.license_plate,
                 'modelo_remolque': vehicle.model_id.name,
-                'capacidad_carga':vehicle.capacidad_carga,
-                'dia_operacion':fecha
+                'capacidad_carga': vehicle.capacidad_carga,
+                'dia_operacion': fecha
             })
 
-            day+=1
+            day += 1
 
         return vehicle
 
-    def suma_annos(self,fecha,cant_annos):
+    def action_create_remolque(self):
+        remolque_dia = self.env['remolque_dia']
+        today = datetime.today().date()
+        end_date = self.suma_annos(today, 2)
+        cant_days = (end_date - today).days
+        vihiculos = self.env['fleet.vehicle'].search([])
+
+        for vehicle in vihiculos:
+            if len(remolque_dia.search(
+                    [('patente_remolque', '=', vehicle.license_plate), ('dia_operacion', '=', str(today))])) > 0:
+                pass
+            else:
+                day = 0
+                while day <= cant_days:
+                    fecha = today + timedelta(days=day)
+                    remolque_dia.create({
+                        'patente_remolque': vehicle.license_plate,
+                        'modelo_remolque': vehicle.model_id.name,
+                        'capacidad_carga': vehicle.capacidad_carga,
+                        'dia_operacion': fecha
+                    })
+
+                    day += 1
+
+    def suma_annos(self, fecha, cant_annos):
         try:
-            return fecha.replace(year = fecha.year + cant_annos)
+            return fecha.replace(year=fecha.year + cant_annos)
         except ValueError:
-            return fecha + (date(fecha.year+cant_annos,1,1)-date(fecha.year,1,1))
+            return fecha + (date(fecha.year + cant_annos, 1, 1) - date(fecha.year, 1, 1))
