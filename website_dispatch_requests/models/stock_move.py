bf4da0c227f1b944497609d3ca3768e06a51e67d
Antonio Ruban <rbn@oxe360.com>
2021-09-02 07:56:40 -0400

    [ADD] Creacion de las ordenes de entrega para cada tarea

--- website_dispatch_requests/models/stock_move.py
+++ website_dispatch_requests/models/stock_move.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from itertools import groupby
 from odoo import fields, models, api, _
 
 
@@ -26,3 +27,35 @@ class StockMove(models.Model):
         for rec in self:
             rec.total_reserved = rec.reserved_availability * rec.line_price_unit
             rec.total_demanded = rec.product_uom_qty * rec.line_price_unit
+
+    def _assign_picking(self):
+        """ Try to assign the moves to an existing picking that has not been
+        reserved yet and has the same procurement group, locations and picking
+        type (moves should already have them identical). Otherwise, create a new
+        picking to assign them to. """
+        Picking = self.env['stock.picking']
+        grouped_moves = groupby(sorted(self, key=lambda m: [f.id for f in m._key_assign_picking()]),
+                                key=lambda m: [m._key_assign_picking()])
+        for group, moves in grouped_moves:
+            moves = self.env['stock.move'].concat(*list(moves))
+            new_picking = False
+            # Could pass the arguments contained in group but they are the same
+            # for each move that why moves[0] is acceptable
+            picking = moves[0]._search_picking_for_assignation()
+            if picking and not self.env.context.get('dispatch_qty', False):
+                if any(picking.partner_id.id != m.partner_id.id or
+                       picking.origin != m.origin for m in moves):
+                    # If a picking is found, we'll append `move` to its move list and thus its
+                    # `partner_id` and `ref` field will refer to multiple records. In this
+                    # case, we chose to  wipe them.
+                    picking.write({
+                        'partner_id': False,
+                        'origin': False,
+                    })
+            else:
+                new_picking = True
+                picking = Picking.create(moves._get_new_picking_values())
+
+            moves.write({'picking_id': picking.id})
+            moves._assign_picking_post_process(new=new_picking)
+        return True
